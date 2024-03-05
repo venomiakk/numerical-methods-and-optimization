7e4ef1ec191df7b253c479ef04600b4178acfe7f
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-03-05 13:44:02 +0100

    sieczne1

--- zad1/src/sieczne.py
+++ zad1/src/sieczne.py
@@ -1,4 +1,8 @@
 import misc
 
-def metoda_siczenych(funkcja, a, b, pochodna):
+def wyznaczanie_pierwiastka(funkcja, xn, xn1):
     
+    return xn - ((misc.horner(xn, funkcja,3)*(xn-xn1))/(misc.horner(xn,funkcja,3)-misc.horner(xn1,funkcja,3)))
+
+def metoda_siczenych(funkcja, a, b, pochodna):
+    return 0
