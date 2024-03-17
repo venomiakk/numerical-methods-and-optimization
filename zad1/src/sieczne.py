285ceecdda214b52795ec40c48b12d7f34d7922f
Adrian <147884387+venomiakk@users.noreply.github.com>
2024-03-17 23:58:33 +0100

    kolejne ciekawe wyniki z siecznych XD

--- zad1/src/sieczne.py
+++ zad1/src/sieczne.py
@@ -68,6 +68,11 @@ def oblicz(a, b, typ, warunek, war_stop):
 # oblicz(0.5, 1.5, 1, 1, 20) 
 #2
 # oblicz(-0.9, 0.6, 2, 1, 20)
+    
+#??
+#2
+# oblicz(-0.8, 0.6, 2, 1, 20)
+# oblicz(-0.8, 0.6, 2, 2, 0.001)
 
 #x^2
 # oblicz(-1, 1, 5, 1, 20)
